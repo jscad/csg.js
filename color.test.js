7fd7fe90c6e3d2503182f2873adc9b0fd71f9166
Z3 Development <z3-dev@gfnews.net>
2017-02-25 17:08:54 +0900

    Expanded cases for 3D color tests

--- color.test.js
+++ color.test.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-import { cube, sphere } from './primitives3d'
+import { cube, sphere, cylinder } from './primitives3d'
 import { square, circle } from './primitives2d'
 import { color, rgb2hsl, hsl2rgb, rgb2hsv, hsv2rgb } from './color'
 
@@ -63,7 +63,7 @@ test('color (rgba, on 2d objects)', t => {
   t.deepEqual(obs.sides[obs.sides.length - 1].shared, expColor)
 })
 
-test('color (rgba, on 3d , array of objects)', t => {
+test('color (rgba, on array of 3D objects)', t => {
   const obs = color([1, 0, 0, 0.5], [cube(), sphere()])
   const expColor = { color: [ 1, 0, 0, 0.5 ] }
 
@@ -80,8 +80,20 @@ test('color (rgba, on array of 2d objects)', t => {
 })
 
 test('color (by name, on 3d objects)', t => {
-  const obs = color('red', cube(), sphere())
-  const expColor = { color: [ 1, 0, 0, 1 ] }
+  var obs = color('red', cube())
+  var expColor = { color: [ 1, 0, 0, 1 ] }
+
+  t.deepEqual(obs.polygons[0].shared, expColor)
+  t.deepEqual(obs.polygons[obs.polygons.length - 1].shared, expColor)
+
+  obs = color('green', cube(), sphere())
+  expColor = { color: [ 0, 128 / 255, 0, 1 ] }
+
+  t.deepEqual(obs.polygons[0].shared, expColor)
+  t.deepEqual(obs.polygons[obs.polygons.length - 1].shared, expColor)
+
+  obs = color('blue', cube(), sphere(), cylinder())
+  expColor = { color: [ 0, 0, 1, 1 ] }
 
   t.deepEqual(obs.polygons[0].shared, expColor)
   t.deepEqual(obs.polygons[obs.polygons.length - 1].shared, expColor)
@@ -96,15 +108,27 @@ test('color (by name, on 2d objects)', t => {
 })
 
 test('color (by name and alpha, on 3d objects)', t => {
-  const obs = color('red', 0.5, cube(), sphere())
-  const expColor = { color: [ 1, 0, 0, 0.5 ] }
+  var obs = color('red', 0.5, cube())
+  var expColor = { color: [ 1, 0, 0, 0.5 ] }
+
+  t.deepEqual(obs.polygons[0].shared, expColor)
+  t.deepEqual(obs.polygons[obs.polygons.length - 1].shared, expColor)
+
+  obs = color('green', 0.8, cube(), sphere())
+  expColor = { color: [ 0, 128 / 255, 0, 0.8 ] }
+
+  t.deepEqual(obs.polygons[0].shared, expColor)
+  t.deepEqual(obs.polygons[obs.polygons.length - 1].shared, expColor)
+
+  obs = color('blue', 0.2, cube(), sphere(), cylinder(())
+  expColor = { color: [ 0, 0, 1, 0.2 ] }
 
   t.deepEqual(obs.polygons[0].shared, expColor)
   t.deepEqual(obs.polygons[obs.polygons.length - 1].shared, expColor)
 })
 
 test('color (by name and alpha, on 2d objects)', t => {
-  const obs = color('red', 0.5, square(), circle())
+  const obs = color('red', 0.1, square(), circle())
   const expColor = { color: [ 1, 0, 0, 0.5 ] }
 
   t.deepEqual(obs.sides[0].shared, expColor)
@@ -112,8 +136,8 @@ test('color (by name and alpha, on 2d objects)', t => {
 })
 
 test('color (by name and alpha, on array of 3d objects)', t => {
-  const obs = color('red', 0.5, [cube(), sphere()])
-  const expColor = { color: [ 1, 0, 0, 0.5 ] }
+  const obs = color('red', 0.7, [cube(), sphere()])
+  const expColor = { color: [ 1, 0, 0, 0.7 ] }
 
   t.deepEqual(obs.polygons[0].shared, expColor)
   t.deepEqual(obs.polygons[obs.polygons.length - 1].shared, expColor)
