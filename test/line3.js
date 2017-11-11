f783a47574a268e8932749b438802b8850cdc2a7
Z3 Development <z3-dev@gfnews.net>
2017-11-11 17:50:36 +0900

    Completed transform and geometry function tests

--- test/line3.js
+++ test/line3.js
@@ -17,7 +17,7 @@ function vector3Equals (t, observed, expected) {
   return t.deepEqual(obs, expected)
 }
 
-test('CSG.Line3 constructors creates valid lines', t => {
+test('CSG.Line3 constructors create valid lines', t => {
   const Line3 = CSG.Line3D
   const Vector3 = CSG.Vector3D
   const Plane = CSG.Plane
@@ -40,5 +40,53 @@ test('CSG.Line3 constructors creates valid lines', t => {
 
   vector3Equals(t,l3.point,[10,0,0])
   vector3Equals(t,l3.direction,[-0,1,0])
+
+  let l4 = l1.clone()
+  t.deepEqual(l4, l1)
+})
+
+test('CSG.Line3 transforms', t => {
+  const Line3 = CSG.Line3D
+  const Vector3 = CSG.Vector3D
+  const Plane = CSG.Plane
+
+  let l1 = Line3.fromPoints([0,0,0],[10,-10,10])
+  vector3Equals(t,l1.point,[0,0,0])
+  vector3Equals(t,l1.direction,[0.5773502691896257,-0.5773502691896257,0.5773502691896257])
+
+  let l2 = l1.reverse()
+  vector3Equals(t,l2.point,[0,0,0])
+  vector3Equals(t,l2.direction,[-0.5773502691896257,0.5773502691896257,-0.5773502691896257])
+
+  let matrix = CSG.Matrix4x4.rotationX(90)
+  matrix = matrix.multiply(CSG.Matrix4x4.translation([-100,0,-100]))
+
+  let l3 = l1.transform(matrix)
+  vector3Equals(t,l3.point,[-100,0,-100])
+  vector3Equals(t,l3.direction,[0.5773502691896247,-0.5773502691896278,-0.5773502691896247])
+})
+
+test('CSG.Line3 geometry calculations', t => {
+  const Line3 = CSG.Line3D
+  const Vector3 = CSG.Vector3D
+  const Plane = CSG.Plane
+
+  let l1 = Line3.fromPoints([-10,-10,-10],[0,0,0])
+  vector3Equals(t,l1.point,[-10,-10,-10])
+  vector3Equals(t,l1.direction,[0.5773502691896257,0.5773502691896257,0.5773502691896257])
+
+  let a = new Vector3(0,0,0)
+  let b = new Vector3(10,10,0)
+  let c = new Vector3(-10,10,0)
+  let p1 = Plane.fromVector3Ds(a,b,c)
+
+  let i1 = l1.intersectWithPlane(p1)
+  vector3Equals(t,i1,[0,0,0])
+
+  let d1 = l1.distanceToPoint([1,1,0])
+  t.is(d1,0.816496580927726)
+
+  let i2 = l1.closestPointOnLine([1,1,0])
+  vector3Equals(t,i2,[0.6666666666666661,0.6666666666666661,0.6666666666666661])
 })
 
