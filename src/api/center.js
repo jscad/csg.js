ebdabfaf70a1dd45ee19d7b9d00c02f88aab8a08
Z3 Development <z3-dev@gfnews.net>
2018-03-07 21:18:01 +0900

    Cleaned up, and added options.center to center upon a given point

--- src/api/center.js
+++ src/api/center.js
@@ -1,24 +1,29 @@
 const toArray = require('../core/utils/toArray')
 
 /**
- * centers the given object(s) on the given axes
- * @param {Object|Array} object(s) the shapes to center
- * @param {Object} options
+ * centers the given object(s) using the given options (if any)
+ * @param {Object} [options] - options for centering
+ * @param {Array} [options.axes=[true,true,true]] - axis of which to center, true or false
+ * @param {Array} [options.center=[0,0,0]] - point of which to center the object upon
+ * @param {Object|Array} objects - the shape(s) to center
+ *
+ * TODO : Add addition 'methods' of centering; midpoint, centeriod
  */
-const centerV2 = function (objects, options) {
+const centerV2 = function (options, objects) {
   const defaults = {
-    axes: [1, 1, 1]
+    axes: [true, true, true],
+    center: [0, 0, 0]
   }
   options = Object.assign({}, defaults, options)
-  const {axes} = options
+  const {axes,center} = options
   objects = toArray(objects)
 
   const results = objects.map(function (object) {
-    let b = object.getBounds()
+    let bounds = object.getBounds()
     let offset = [0,0,0]
-    if (axes[0]) offset[0] = 0 - (b[0]._x + ((b[1]._x - b[0]._x) / 2))
-    if (axes[1]) offset[1] = 0 - (b[0]._y + ((b[1]._y - b[0]._y) / 2))
-    if (axes[2]) offset[2] = 0 - (b[0]._z + ((b[1]._y - b[0]._y) / 2))
+    if (axes[0]) offset[0] = center[0] - (bounds[0].x + ((bounds[1].x - bounds[0].x) / 2))
+    if (axes[1]) offset[1] = center[1] - (bounds[0].y + ((bounds[1].y - bounds[0].y) / 2))
+    if (axes[2]) offset[2] = center[2] - (bounds[0].z + ((bounds[1].y - bounds[0].y) / 2))
     return object.translate(offset)
   })
   // if there is more than one result, return them all , otherwise a single one
