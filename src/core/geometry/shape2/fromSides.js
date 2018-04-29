f1daee3be92b2414c07fbd91e7f04cfc2c5f949b
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-04-29 14:26:51 +0200

    feat(shape2/shape3): added creation helpers * updated 'from sides' to use the helper

--- src/core/geometry/shape2/fromSides.js
+++ src/core/geometry/shape2/fromSides.js
@@ -1,13 +1,13 @@
+const create = require('./create')
 /** Construct a CAG from a list of `Side` instances.
  * this is a duplicate of CAG's fromSides to avoid circular dependency CAG => fromSides => CAG
  * @param {Side[]} sides - list of sides
  * @returns {CAG} new CAG object
  */
 const fromSides = function (sides) {
-  const CAG = require('./CAG')
-  let cag = new CAG()
-  cag.sides = sides
-  return cag
+  let shape2 = create()
+  shape2.sides = sides
+  return shape2
 }
 
 module.exports = fromSides
---
+++ src/core/geometry/shape3/create.js
@@ -0,0 +1,18 @@
+const Properties = require('./Properties')
+
+/** create shape3/ CSG
+ * Holds a binary space partition tree representing a 3D solid. Two solids can
+ * be combined using the `union()`, `subtract()`, and `intersect()` methods.
+ * @constructor
+ */
+const create = function () {
+  return {
+    type: 'shape3',
+    polygons: [],
+    properties: new Properties(),
+    isCanonicalized: true,
+    isRetesselated: true
+  }
+}
+
+module.exports = create
