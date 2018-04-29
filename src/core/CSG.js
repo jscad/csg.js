db12ccbac1bf742f0dae0ab899174863df5d097f
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-04-29 14:28:04 +0200

    feat(csg): added rotateEuler angles (temporary)

--- src/core/CSG.js
+++ src/core/CSG.js
@@ -137,6 +137,17 @@ CSG.prototype = {
     return this.transform(Matrix4x4.rotation(rotationCenter, rotationAxis, degrees))
   },
 
+  rotateEulerAngles: function (alpha, beta, gamma, position) {
+    position = position || [0, 0, 0]
+
+    let Rz1 = Matrix4x4.rotationZ(alpha)
+    let Rx = Matrix4x4.rotationX(beta)
+    let Rz2 = Matrix4x4.rotationZ(gamma)
+    let T = Matrix4x4.translation(new Vector3D(position))
+
+    return this.transform(Rz2.multiply(Rx).multiply(Rz1).multiply(T))
+  },
+
   mirrored: function (plane) {
     return this.transform(Matrix4x4.mirroring(plane))
   },
