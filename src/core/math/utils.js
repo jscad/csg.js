ebc4ada72aba3fe4485b04751b549ea367b47a6d
Z3 Development <z3-dev@gfnews.net>
2018-07-29 09:55:24 +0900

    fix() : removed isFloat

--- src/core/math/utils.js
+++ src/core/math/utils.js
@@ -1,8 +1,4 @@
 const radToDeg = radians => radians * 114.59155903
 const degToRad = degrees => degrees * 0.00872665
 
-const IsFloat = function (n) {
-  return ! (isNaN(n) || (n === Infinity) || (n === -Infinity))
-}
-
-module.exports = {radToDeg, degToRad, IsFloat}
+module.exports = {radToDeg, degToRad}
