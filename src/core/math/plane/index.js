27606ebc64fa58e568762f0c2bc9e5d92f2cd75c
Z3 Development <z3-dev@gfnews.net>
2018-08-11 13:05:09 +0900

    fix() : updated plane.index to use vec4 when possible

--- src/core/math/plane/index.js
+++ src/core/math/plane/index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  create: require('./create'),
+  clone: require('../vec4/clone'),
+  create: require('../vec4/create'),
   equals: require('./equals'),
   flip: require('./flip'),
   fromData: require('./fromData'),
@@ -8,6 +9,6 @@ module.exports = {
   fromPoints: require('./fromPoints'),
   fromVector3Ds: require('./fromVector3Ds'),
   signedDistanceToPoint: require('./signedDistanceToPoint'),
-  toString: require('./toString'),
+  toString: require('../vec4/toString'),
   transform: require('./transform'),
 }
