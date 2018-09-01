8f6e400769678d0eec8af78c9c687eaefc3a9433
Z3 Development <z3-dev@gfnews.net>
2018-09-01 16:24:05 +0900

    fix() : correct vec3.random() to use abs() for determination of order

--- src/core/math/vec3/random.js
+++ src/core/math/vec3/random.js
@@ -1,13 +1,13 @@
 module.exports = random
 
-const normalize = require('./normalize')
+const abs = require('./abs')
 const fromValues = require('./fromValues')
 
 function random (input) {
-  let abs = normalize(input)
-  if ((abs[0] <= abs[1]) && (abs[0] <= abs[2])) {
+  let vec = abs(input)
+  if ((vec[0] <= vec[1]) && (vec[0] <= vec[2])) {
     return fromValues(1, 0, 0)
-  } else if ((abs[1] <= abs[0]) && (abs[1] <= abs[2])) {
+  } else if ((vec[1] <= vec[0]) && (vec[1] <= vec[2])) {
     return fromValues(0, 1, 0)
   } else {
     return fromValues(0, 0, 1)
