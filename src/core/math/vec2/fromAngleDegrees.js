459595ae64db543c910f2dbae44cfd6ae539938c
Z3 Development <z3-dev@gfnews.net>
2018-09-09 17:46:16 +0900

    fix() corrected to use create in fromAngleDegrees and fromAngleRadians

--- src/core/math/vec2/fromAngleDegrees.js
+++ src/core/math/vec2/fromAngleDegrees.js
@@ -1,6 +1,8 @@
+const create = require('./create')
+
 const fromAngleDegrees = (degrees) => {
   const radians = Math.PI * degrees / 180
-  let out = new Float32Array(3)
+  let out = create()
   out[0] = Math.cos(radians)
   out[1] = Math.sin(radians)
   return out
--- src/core/math/vec2/fromAngleRadians.js
+++ src/core/math/vec2/fromAngleRadians.js
@@ -1,5 +1,7 @@
+const create = require('./create')
+
 const fromAngleRadians = radians => {
-  let out = new Float32Array(3)
+  let out = create()
   out[0] = Math.cos(radians)
   out[1] = Math.sin(radians)
   return out
