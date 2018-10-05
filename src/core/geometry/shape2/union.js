4c4e95c89523f023919897ec97aab231e7e3dfc0
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-10-05 15:19:56 +0200

    refactor(): experimenting with data structures/nesting

--- src/core/geometry/shape2/union.js
+++ src/core/geometry/shape2/union.js
@@ -4,6 +4,31 @@ const toShape3Wall = require('./toShape3Wall')
 const fromFakeShape3 = require('./fromFakeShape3')
 const transformGeometry = require('./transformGeometry')
 
+// FIXME: duplicate code in shape3.union
+const unionSub = function (otherCsg, csg, doRetesselate, doCanonicalize) {
+  if (!isOverlapping(otherCsg, csg)) {
+    return unionForNonIntersecting(otherCsg, csg)
+  } else {
+    let a = new Tree(otherCsg.polygons)
+    let b = new Tree(csg.polygons)
+    a.clipTo(b, false)
+
+    // b.clipTo(a, true); // ERROR: this doesn't work
+    b.clipTo(a)
+    b.invert()
+    b.clipTo(a)
+    b.invert()
+
+    let newpolygons = a.allPolygons().concat(b.allPolygons())
+    let result = fromPolygons(newpolygons)
+    // FIXME: what to do with properties ????
+    // result.properties = otherCsg.properties._merge(csg.properties)
+    if (doRetesselate) result = retesselate(result)
+    if (doCanonicalize) result = canonicalize(result)
+    return result
+  }
+}
+
 /** // FIXME: double check this algorithm, or even better, swap it out with something not reliant
  * on converting to 3D and back !!!
  * do a boolean union of two (or more) 2d shapes
