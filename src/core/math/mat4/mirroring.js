b62c055648d686dbbf4f9e508a03ca936bdc6d01
Z3 Development <z3-dev@gfnews.net>
2018-10-06 17:47:17 +0900

    fix(mat4) corrected mirroring to accept one or two params

--- src/core/math/mat4/mirroring.js
+++ src/core/math/mat4/mirroring.js
@@ -2,35 +2,42 @@ const create = require('./create')
 const fromValues = require('./fromValues')
 
 /**
- * m the mat4 by the dimensions in the given vec3
- * create an affine matrix for mirroring into an arbitrary plane:
+ * Create an affine matrix for mirroring onto an arbitrary plane
  *
- * @param {mat4} out the receiving matrix
- * @param {mat4} a the matrix to mirror
- * @param {vec4} plane the vec4 to mirror the matrix by
+ * @param {mat4} [out] receiving matrix
+ * @param {vec4} plane to mirror the matrix by
  * @returns {mat4} out
  */
 const mirroring = (...params) => {
   let out
-  let a
   let plane
-  if (params.length === 2) {
+  if (params.length === 1) {
     out = create()
     plane = params[0]
-    a = params[1]
   } else {
     out = params[0]
     plane = params[1]
-    a = params[2]
   }
   const [nx, ny, nz, w] = plane
-  const elements = [
-    (1.0 - 2.0 * nx * nx), (-2.0 * ny * nx), (-2.0 * nz * nx), 0,
-    (-2.0 * nx * ny), (1.0 - 2.0 * ny * ny), (-2.0 * nz * ny), 0,
-    (-2.0 * nx * nz), (-2.0 * ny * nz), (1.0 - 2.0 * nz * nz), 0,
-    (2.0 * nx * w), (2.0 * ny * w), (2.0 * nz * w), 1
-  ]
-  return fromValues(...elements)
+
+  out[0] = (1.0 - 2.0 * nx * nx)
+  out[1] = (-2.0 * ny * nx)
+  out[2] = (-2.0 * nz * nx)
+  out[3] = 0
+  out[4] = (-2.0 * nx * ny)
+  out[5] = (1.0 - 2.0 * ny * ny)
+  out[6] = (-2.0 * nz * ny)
+  out[7] = 0
+  out[8] = (-2.0 * nx * nz)
+  out[9] = (-2.0 * ny * nz)
+  out[10] = (1.0 - 2.0 * nz * nz)
+  out[11] = 0
+  out[12] = (2.0 * nx * w)
+  out[13] = (2.0 * ny * w)
+  out[14] = (2.0 * nz * w)
+  out[15] = 1
+
+  return out
 }
 
 module.exports = mirroring
