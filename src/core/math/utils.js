786d72c1e691c92300ef9a2d34e3fd586a78f04d
Z3 Development <z3-dev@gfnews.net>
2018-10-28 17:47:24 +0900

    fix(utils) added solve2linear to math utils

--- src/core/math/utils.js
+++ src/core/math/utils.js
@@ -5,4 +5,14 @@ const degToRad = degrees => degrees * 0.017453292519943295
 
 const clamp = (value, min, max) => Math.min(Math.max(value, min), max)
 
-module.exports = { radToDeg, degToRad, clamp }
+const solve2Linear = function (a, b, c, d, u, v) {
+  let det = a * d - b * c
+  let invdet = 1.0 / det
+  let x = u * d - b * v
+  let y = -u * c + a * v
+  x *= invdet
+  y *= invdet
+  return [x, y]
+}
+
+module.exports = { radToDeg, degToRad, clamp, solve2Linear }
