d53d45df57bb0346f842667e59035251a88ef315
Z3 Development <z3-dev@gfnews.net>
2018-12-25 16:32:53 +0900

    refactor() added toString function to line2

---
+++ src/core/math/line2/toString.js
@@ -0,0 +1,7 @@
+
+const toString = (line) => {
+  //return `line2: (${line[0].toFixed(7)}, ${line[1].toFixed(7)}, ${line[2].toFixed(7)})`
+  return `line2: (${line[0]}, ${line[1]}, ${line[2]})`
+}
+
+module.exports = toString
