44948a9697d822cf793d2ec8793e5d30bdecb2fe
Z3 Development <z3-dev@gfnews.net>
2018-12-29 15:06:28 +0900

    fix(line2) corrected closestPoint and added test suite

--- src/core/math/line2/closestPoint.js
+++ src/core/math/line2/closestPoint.js
@@ -1,30 +1,26 @@
 const vec2 = require('../vec2')
-const { solve2Linear } = require("../../utils/various")
 
 const direction = require('./direction')
 const origin = require('./origin')
-const fromPoints = require('./fromPoints')
-
-const closestPoint = (line, point) => {
-  // distance to line (closest, 90 degress from line)
-  // distance to origin
-  // if same then closest is origin
-  // else calculate distance of last side
-console.log('***** closestPoint')
-  line2 = fromPoints(vec2.rotate(Math.PI, point), point)
-console.log(vec2.toString(line2))
-  let closest = solve2Linear(line[0], line[1], line2[0], line2[1], line[2], line2[2])
-  return closest
-}
 
 /**
- * FIXME Is this correct?
+ * Determine the closest point on the given line to the given point
+ * Thanks to @khrismuc
  */
-const b = () => {
-  const org = origin(line)
-  const dir = direction(line)
-  const distance = vec2.dot(point, dir)
-  const closest = vec2.add(org, [distance, distance])
+const closestPoint = (point, line) => {
+  // linear function of AB
+  const a = origin(line)
+  const b = direction(line)
+  const m1 = (b[1] - a[1]) / (b[0] - a[0])
+  const t1 = a[1] - m1 * a[0]
+  // linear function of PC
+  const m2 = -1 / m1 // perpendicular
+  const t2 = point[1] - m2 * point[0]
+  // c.x * m1 + t1 === c.x * m2 + t2
+  const x = (t2 - t1) / (m1 - m2)
+  const y = m1 * x + t1
+
+  const closest = vec2.fromValues(x, y)
   return closest
 }
 
