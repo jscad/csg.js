c3ed6c34845f8b4d3755affb71275d2b74075211
Z3 Development <z3-dev@gfnews.net>
2018-12-31 14:10:05 +0900

    fix(line2) added optional out parameter to reverse, and added tests

--- src/core/math/line2/reverse.js
+++ src/core/math/line2/reverse.js
@@ -1,17 +1,30 @@
 const vec2 = require('../vec2')
 
+const clone = require('./clone')
+const create = require('./create')
 const fromValues = require('./fromValues')
 
 /**
  * Create a new line in the opposite direction as the given.
  *
+ * @param {line2} [out] - receiving line
  * @param {line2} line the 2D line to reverse
  * @returns {line2} a new unbounded 2D line
  */
-const reverse = (line) => {
+const reverse = (...params) => {
+  let out
+  let line
+  if (params.length === 1) {
+    out = create()
+    line = params[0]
+  } else {
+    out = params[0]
+    line = params[1]
+  }
+
   let normal = vec2.negate(line)
   let distance = -line[2]
-  return fromValues(normal[0], normal[1], distance)
+  return clone(out, fromValues(normal[0], normal[1], distance))
 }
 
 module.exports = reverse
--- src/core/math/line2/reverse.test.js
+++ src/core/math/line2/reverse.test.js
@@ -24,3 +24,36 @@ test('line2: reverse() should return proper lines', (t) => {
   const rev5 = reverse(line5)
   t.true(compareVectors(rev5, [-0.7071067690849304, -0.7071067690849304, 0]))
 })
+
+test('line2: reverse() called with two paramerters should update a line2 with proper values', (t) => {
+  const line1 = create()
+  const obs1 = create()
+  const ret1 = reverse(obs1, line1)
+  t.true(compareVectors(ret1, [0, -1, 0]))
+  t.true(compareVectors(obs1, [0, -1, 0]))
+
+  const line2 = fromPoints([1, 0], [0, 1])
+  const obs2 = create()
+  const ret2 = reverse(obs2, line2)
+  t.true(compareVectors(ret2, [0.7071067690849304, 0.7071067690849304, 0.7071067690849304]))
+  t.true(compareVectors(obs2, [0.7071067690849304, 0.7071067690849304, 0.7071067690849304]))
+
+  const line3 = fromPoints([0, 1], [1, 0])
+  const obs3 = create()
+  const ret3 = reverse(obs3, line3)
+  t.true(compareVectors(ret3, [-0.7071067690849304, -0.7071067690849304, -0.7071067690849304]))
+  t.true(compareVectors(obs3, [-0.7071067690849304, -0.7071067690849304, -0.7071067690849304]))
+
+  const line4 = fromPoints([0, 6], [6, 0])
+  const obs4 = create()
+  const ret4 = reverse(obs4, line4)
+  t.true(compareVectors(ret4, [-0.7071067690849304, -0.7071067690849304, -4.242640495300293]))
+  t.true(compareVectors(obs4, [-0.7071067690849304, -0.7071067690849304, -4.242640495300293]))
+
+  const line5 = fromPoints([-5, 5], [5, -5])
+  const obs5 = create()
+  const ret5 = reverse(obs5, line5)
+  t.true(compareVectors(ret5, [-0.7071067690849304, -0.7071067690849304, 0]))
+  t.true(compareVectors(obs5, [-0.7071067690849304, -0.7071067690849304, 0]))
+})
+
