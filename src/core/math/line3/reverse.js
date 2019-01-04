1774202610e0be2996a4c7911efcb02252a89493
Z3 Development <z3-dev@gfnews.net>
2019-01-04 12:01:45 +0900

    fix(line3) add optional out parameter to reverse, and added tests

--- src/core/math/line3/reverse.js
+++ src/core/math/line3/reverse.js
@@ -1,5 +1,7 @@
 const vec3 = require('../vec3')
 
+const clone = require('./clone')
+const create = require('./create')
 const fromData = require('./fromData')
 
 /**
@@ -9,10 +11,20 @@ const fromData = require('./fromData')
  * @param {line3} line the 3D line to reverse
  * @returns {line3} a new unbounded 3D line
  */
-const reverse = (line) => {
+const reverse = (...params) => {
+  let out
+  let line
+  if (params.length === 1) {
+    out = create()
+    line = params[0]
+  } else {
+    out = params[0]
+    line = params[1]
+  }
+
   let point = vec3.clone(line[0])
   let direction = vec3.negate(line[1])
-  return fromData(point, direction)
+  return clone(out, fromData(point, direction))
 }
 
 module.exports = reverse
--- src/core/math/line3/reverse.test.js
+++ src/core/math/line3/reverse.test.js
@@ -3,7 +3,7 @@ const { reverse, create, fromPoints } = require('./index')
 
 const { compareVectors } = require('../../../../test/helpers/index')
 
-test.only('line3: reverse() should return proper lines', (t) => {
+test('line3: reverse() should return proper lines', (t) => {
   const line1 = create()
   let rev = reverse(line1)
   let pnt = rev[0]
@@ -42,33 +42,30 @@ test.only('line3: reverse() should return proper lines', (t) => {
 
 test('line3: reverse() called with two paramerters should update a line3 with proper values', (t) => {
   const line1 = create()
-  const obs1 = create()
-  const ret1 = reverse(obs1, line1)
-  t.true(compareVectors(ret1, [0, -1, 0]))
-  t.true(compareVectors(obs1, [0, -1, 0]))
+  let out = create()
+  let rev = reverse(out, line1)
+  let pnt = rev[0]
+  let dir = rev[1]
+  t.true(compareVectors(pnt, [0, 0, 0]))
+  t.true(compareVectors(dir, [0, 0, -1]))
+  pnt = out[0]
+  dir = out[1]
+  t.true(compareVectors(pnt, [0, 0, 0]))
+  t.true(compareVectors(dir, [0, 0, -1]))
+  t.not(line1, out)
+  t.is(rev, out)
 
-  const line2 = fromPoints([1, 0], [0, 1])
-  const obs2 = create()
-  const ret2 = reverse(obs2, line2)
-  t.true(compareVectors(ret2, [0.7071067690849304, 0.7071067690849304, 0.7071067690849304]))
-  t.true(compareVectors(obs2, [0.7071067690849304, 0.7071067690849304, 0.7071067690849304]))
-
-  const line3 = fromPoints([0, 1], [1, 0])
-  const obs3 = create()
-  const ret3 = reverse(obs3, line3)
-  t.true(compareVectors(ret3, [-0.7071067690849304, -0.7071067690849304, -0.7071067690849304]))
-  t.true(compareVectors(obs3, [-0.7071067690849304, -0.7071067690849304, -0.7071067690849304]))
-
-  const line4 = fromPoints([0, 6], [6, 0])
-  const obs4 = create()
-  const ret4 = reverse(obs4, line4)
-  t.true(compareVectors(ret4, [-0.7071067690849304, -0.7071067690849304, -4.242640495300293]))
-  t.true(compareVectors(obs4, [-0.7071067690849304, -0.7071067690849304, -4.242640495300293]))
-
-  const line5 = fromPoints([-5, 5], [5, -5])
-  const obs5 = create()
-  const ret5 = reverse(obs5, line5)
-  t.true(compareVectors(ret5, [-0.7071067690849304, -0.7071067690849304, 0]))
-  t.true(compareVectors(obs5, [-0.7071067690849304, -0.7071067690849304, 0]))
+  // reverse in place
+  const line2 = fromPoints([1, 0, 0], [0, 1, 0])
+  rev = reverse(line2, line2)
+  pnt = rev[0]
+  dir = rev[1]
+  t.true(compareVectors(pnt, [1, 0, 0]))
+  t.true(compareVectors(dir, [0.7071067690849304, -0.7071067690849304, 0]))
+  pnt = line2[0]
+  dir = line2[1]
+  t.true(compareVectors(pnt, [1, 0, 0]))
+  t.true(compareVectors(dir, [0.7071067690849304, -0.7071067690849304, 0]))
+  t.is(rev, line2)
 })
 
