423a37aa6a9dbbb6835140e8365b5c0e8b9265ce
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-02-02 23:27:07 +0900

    eslint

--- src/core/shape2/chainHull.js
+++ src/core/shape2/chainHull.js
@@ -3,31 +3,35 @@ const union = require('./union')
 
 // TODO: This is not a primitive operation. Consider moving it to a library.
 
-/** create a chain hull of the given shapes
- * Originally "Whosa whatsis" suggested "Chain Hull" ,
- * as described at https://plus.google.com/u/0/105535247347788377245/posts/aZGXKFX1ACN
- * essentially chainHull({}, A, B, C) is union(hull(A, B), hull(B, C)),
- * and chainHull({ closed: true }, A, B, C) is union(hull(A, B), hull(B, C), hull(C, A).
+/** Create a chain hull of the given shapes.
  *
- * @param {options} geometries either a single or multiple Geom2 objects to create a chain hull around
- * @param {...geometries} a sequence of 
- * @returns {shape} new shape2, being a union of the chained hull operations.
+ * Originally "Whosa whatsis" suggested "Chain Hull", as described at
+ * https://plus.google.com/u/0/105535247347788377245/posts/aZGXKFX1ACN
+ *
+ * e.g.,
+ * chainHull({}, A, B, C) is union(hull(A, B), hull(B, C)),
+ * chainHull({ closed: true }, A, B, C) is union(hull(A, B), hull(B, C), hull(C, A).
+ *
+ * @param {Object} options - Options for hull.
+ * @param {Boolean} options.closed - Closed chains hull the first and last shapes together.
+ * @param {Array<shape2>} shapes - The shapes to sequentially hull together.
+ * @returns {shape2} - The shape of the union of the chained hull operations.
  *
  * @example:
- * let hulled = chainHull({}, rect({ size: [1, 1] }), circle({ r: 1 }))
+ * let hulled = chainHull({}, a, b, c);
  */
 
-const chainHull = ({ closed=false }, ...geometries) => {
-  const hulls = [];
-  const first = 0;
-  const last = geometries.length - 1;
+const chainHull = ({ closed = false }, ...geometries) => {
+  const hulls = []
+  const first = 0
+  const last = geometries.length - 1
   for (let nth = 0; nth < last; nth++) {
-    hulls.push(hull(geometries[nth], geometries[nth + 1]));
+    hulls.push(hull(geometries[nth], geometries[nth + 1]))
   }
   if (closed) {
-    hulls.push(hull(geometries[last], geometries[first]));
+    hulls.push(hull(geometries[last], geometries[first]))
   }
-  return union(...hulls);
+  return union(...hulls)
 }
 
 module.exports = chainHull
--- src/core/shape2/chainHull.test.js
+++ src/core/shape2/chainHull.test.js
@@ -1,36 +1,36 @@
 const test = require('ava')
-const rewiremock = require('rewiremock').default;
+const rewiremock = require('rewiremock').default
 
 test('chainHull: Should hull expected pairs for open chain', t => {
   rewiremock('./hull').with((...args) => ['hull', ...args])
   rewiremock('./union').with((...args) => ['union', ...args])
 
-  rewiremock.enable();
-  const chainHull = require('./chainHull');
-  const implicit = chainHull({}, 'a', 'b', 'c');
-  const explicit = chainHull({ closed: false }, 'a', 'b', 'c');
-  rewiremock.disable();
+  rewiremock.enable()
+  const chainHull = require('./chainHull')
+  const implicit = chainHull({}, 'a', 'b', 'c')
+  const explicit = chainHull({ closed: false }, 'a', 'b', 'c')
+  rewiremock.disable()
 
   // Demonstrate that the pairs are chained sequentially without closing.
   t.deepEqual(explicit, ['union', ['hull', 'a', 'b'],
-                                  ['hull', 'b', 'c']])
+    ['hull', 'b', 'c']])
 
   // Demonstrate that the default is an open chain.
-  t.deepEqual(explicit, implicit);
+  t.deepEqual(explicit, implicit)
 })
 
 test('chainHull: Should hull expected pairs for closed chain', t => {
   rewiremock('./hull').with((...args) => ['hull', ...args])
   rewiremock('./union').with((...args) => ['union', ...args])
 
-  rewiremock.enable();
-  const chainHull = require('./chainHull');
-  const obs = chainHull({ closed: true }, 'a', 'b', 'c');
-  rewiremock.disable();
+  rewiremock.enable()
+  const chainHull = require('./chainHull')
+  const obs = chainHull({ closed: true }, 'a', 'b', 'c')
+  rewiremock.disable()
 
-  t.deepEqual(JSON.stringify(obs), '["union",["hull","a","b"],["hull","b","c"],["hull","c","a"]]');
+  t.deepEqual(JSON.stringify(obs), '["union",["hull","a","b"],["hull","b","c"],["hull","c","a"]]')
 
-  t.deepEqual(obs.length, 4);
+  t.deepEqual(obs.length, 4)
 
   // Demonstrate that the pairs are chained sequentially with closing.
   // t.deepEqual(obs, ['union', ['hull', 'a', 'b'],
