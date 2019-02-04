0d7420a3db0dacc229e90de4ee1605db95283ece
Z3 Development <z3-dev@gfnews.net>
2019-02-04 12:18:49 +0900

    fix(geom3) changed toString to use template strings

--- src/core/geometry/geom3/toString.js
+++ src/core/geometry/geom3/toString.js
@@ -4,7 +4,7 @@ const toString = (geometry) => {
   return geometry.polygons.reduce((result, polygon) => {
     result += poly3.toString(polygon)
     return result
-  }, 'Geom3: (' + geometry.isCanonicalized + ',' + geometry.isRetesselated + ')\n')
+  }, `Geom3: (${geometry.isCanonicalized},${geometry.isRetesselated})\n`)
 }
 
 module.exports = toString
