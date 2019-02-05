c47e9100888e9b8239e8a1ca59a8c6ff9cbee57e
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-02-05 22:38:53 +0900

    More files.

---
+++ src/core/geometry/path2/fromPointArray.js
@@ -0,0 +1,22 @@
+const create = require('./create')
+
+/**
+ * Create a path2 from a point array.
+ * The point array is an array of points, where a point is an array of two
+ *   numbers.
+ * @param {PointArray} pointArray - array of points to create the path from.
+ * @param {boolean} options.closed - if the path should be open or closed.
+ * @example:
+ * path2.fromPointArray({ closed: true }, [[10,10], [-10,10]])
+ */
+const fromPointArray = ({ closed = false }, pointArray) => {
+  let path = create();
+  if (pointArray.length > 0) {
+    path.isCanonicalized = false; // Can't expect these to be canonical points.
+  }
+  path.isClosed = closed;
+  path.points = pointArray.slice(); // Can't expect caller not to modify this.
+  return path;
+}
+
+module.exports = fromPointArray
---
+++ src/core/geometry/path2/toPointArray.js
@@ -0,0 +1,16 @@
+const canonicalize = require('./canonicalize')
+
+/**
+ * Produces a new array containing the path's point data.
+ * This will be an array of canonicalized vec2.
+ * @param {path2} path - the path to canonicalize.
+ * @returns {Array<vec2>} - the array of canonicalized vec2.
+ * @example
+ * toPointArray(path)
+ */
+const toPointArray = path => {
+  // Make a copy, since the caller might modify the point array.
+  return canonicalize(path).points.slice();
+}
+
+module.exports = toPointArray
