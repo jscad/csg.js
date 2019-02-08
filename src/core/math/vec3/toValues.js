1bf37a5527e6baf7115551a13f3b1316aca98afa
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-02-08 12:29:09 +0900

    Document vec3.toValues.

--- src/core/math/vec3/toValues.js
+++ src/core/math/vec3/toValues.js
@@ -1,3 +1,10 @@
+/**
+ * Creates values from a vec3 suitable for creating an equivalent vec3 via
+ *   vec3.fromValues.
+ *
+ * @param {vec3} vector - the vector to create the values from.
+ * @returns {Array<Number>} An array of three numbers.
+ */
 const toValues = vector => [vector[0], vector[1], vector[2]]
 
 module.exports = toValues
