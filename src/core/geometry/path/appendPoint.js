b1d5292c8da40a72e68e5293c9f6025135f67319
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-02-09 16:50:55 +0900

    Improved jsdoc for clone.

--- src/core/geometry/path/appendPoint.js
+++ src/core/geometry/path/appendPoint.js
@@ -5,8 +5,8 @@ const normalizePoint = require('./normalizePoint')
  * Appends a point to an open path.
  * @param {Object} options - options to describe the path to point.
  * @param {vec2|vec3} point - the waypoint to append to the path.
- * @param {path} path - the path appended to
- * @returns {path}
+ * @param {path} path - the path appended to.
+ * @returns {path} the new path produced.
  * @example
  * appendPoint({}, vec3.fromValues(1, 1, 0), path.fromPointArray([[0, 0, 0]]))
  * (should produce [[0, 0, 0], [1, 1, 0]])
