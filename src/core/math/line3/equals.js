fee499422448cdc7af952b88432fe4768e488f1f
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-02-10 11:40:45 +0900

    Add fix note for line3.

--- src/core/math/line3/equals.js
+++ src/core/math/line3/equals.js
@@ -3,13 +3,16 @@ const vec3 = require('../vec3')
 /**
  * Compare the given 3D lines for equality
  *
+ * FIX: This does not compare lines for equality.
+ *
  * @return {boolean} true if lines are equal
  */
 const equals = (line1, line2) => {
   // compare directions (unit vectors)
   if (!vec3.equals(line1[1], line2[1])) return false
 
-  // compare points
+  // FIX: This is wrong -- it will compare false for any two distinct points
+  // on the line.
   if (!vec3.equals(line1[0], line2[0])) return false
 
   // why would lines with the same slope (direction) and different points be equal?
