261c059cff53732d923443290ab190d2d9364fa5
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-02-12 21:23:34 +0900

    Resync.

--- src/api/registry/toImplementation.test.js
+++ src/api/registry/toImplementation.test.js
@@ -1,18 +1,18 @@
-const getImplementation = require('./getImplementation')
+const toImplementation = require('./toImplementation')
 const registerImplementation = require('./registerImplementation')
 const test = require('ava')
 
-test('implementationOf is idempotent', t => {
+test('toImplementation is idempotent', t => {
   const tag = 'tag'
   const module = {}
   const object = { __jscadTag__: tag }
 
   t.is(registerImplementation(tag, module), module)
-  t.is(getImplementation(object), module)
+  t.is(toImplementation(object), module)
 })
 
 test('implementationOf defaults to a consistent null module', t => {
-  const module = getImplementation({ __jscadTag__: 'tag_1' })
+  const module = toImplementation({ __jscadTag__: 'tag_1' })
 
-  t.is(getImplementation({ __jscadTag__: 'tag_2'}), module)
+  t.is(toImplementation({ __jscadTag__: 'tag_2'}), module)
 })
