fb68ebdeaf46e387e252a49e86481b2a661b32f3
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-02-13 13:23:48 +0900

    Update arc generation.

--- src/algorithm/arc/buildSvgArc.test.js
+++ src/algorithm/arc/buildSvgArc.test.js
@@ -1,16 +1,16 @@
+const buildSvgArc = require('./buildSvgArc')
 const test = require('ava')
-const toPointsSvg = require('./toPointsSvg')
 const vec2 = require('../../math/vec2')
 
 // TODO: More comprehensive test cases.
 
 test('Flat arc', t => {
-  t.deepEqual(toPointsSvg({}, [0, 0], [1, 1]).map(p => [...p]),
+  t.deepEqual(buildSvgArc({}, [0, 0], [1, 1]).map(p => [...p]),
               [[0, 0], [1, 1]])
 })
 
 test('Half circle, small arc', t => {
-  t.deepEqual(toPointsSvg({ resolution: 6, large: false, xRadius: 0.5, yRadius: 0.5 }, [1, 1], [2, 2]).map(p => [...p]),
+  t.deepEqual(buildSvgArc({ resolution: 6, large: false, xRadius: 0.5, yRadius: 0.5 }, [1, 1], [2, 2]).map(p => [...p]),
               [[1, 1],
                [1.499559998512268, 0.7943999767303467],
                [1.9984899759292603, 1.0015100240707397],
@@ -19,7 +19,7 @@ test('Half circle, small arc', t => {
 })
 
 test('Half circle, large arc', t => {
-  t.deepEqual(toPointsSvg({ resolution: 6, large: true, xRadius: 0.5, yRadius: 0.5 }, [1, 1], [2, 2]).map(p => [...p]),
+  t.deepEqual(buildSvgArc({ resolution: 6, large: true, xRadius: 0.5, yRadius: 0.5 }, [1, 1], [2, 2]).map(p => [...p]),
               [[1, 1],
                [1.3896299600601196, 0.8002899885177612],
                [1.8221499919891357, 0.8682600259780884],
@@ -29,7 +29,7 @@ test('Half circle, large arc', t => {
 })
 
 test('Quarter circle', t => {
-  t.deepEqual(toPointsSvg({ resolution: 6, large: false, xRadius: 1, yRadius: 1 }, [1, 1], [2, 2]).map(p => [...p]),
+  t.deepEqual(buildSvgArc({ resolution: 6, large: false, xRadius: 1, yRadius: 1 }, [1, 1], [2, 2]).map(p => [...p]),
               [[1, 1],
                [1.5, 1.133970022201538],
                [1.866029977798462, 1.5],
@@ -37,7 +37,7 @@ test('Quarter circle', t => {
 })
 
 test('Three-quarter circle', t => {
-  t.deepEqual(toPointsSvg({ resolution: 6, large: true, xRadius: 1, yRadius: 1 }, [1, 1], [2, 2]).map(p => [...p]),
+  t.deepEqual(buildSvgArc({ resolution: 6, large: true, xRadius: 1, yRadius: 1 }, [1, 1], [2, 2]).map(p => [...p]),
               [[1, 1],
                [1.292889952659607, 0.2928900122642517],
                [2, 0],
