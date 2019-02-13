8994457f235587458c20f29b2305a1a89f04a524
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-02-13 11:23:30 +0900

    More leakage.

--- src/geometry/path/index.js
+++ src/geometry/path/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   appendPoint: require('./appendPoint'),
   canonicalize: require('./canonicalize'),
-  close: require('./close'),
   concat: require('./concat'),
   create: require('./create'),
   eachPoint: require('./eachPoint'),
