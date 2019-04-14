b455ec9c5b05c852ee8df27b9ce7d8875f725af7
Z3 Development <z3-dev@gfnews.net>
2019-04-14 13:33:29 +0900

    fix(path2) expanded attribute and type checks in isA

--- src/geometry/path2/isA.js
+++ src/geometry/path2/isA.js
@@ -1,12 +1,14 @@
 /**
- * Determin if the given object is a 2D geometry.
- * @params {geom2} object - the object to interogate
- * @returns {true} if the object matches a geom2 based object
+ * Determin if the given object is a path2 geometry.
+ * @params {object} object - the object to interogate
+ * @returns {true} if the object matches a path2 based object
  */
 const isA = (object) => {
   if (object && typeof object === 'object') {
-    if ('basePoints' in object && Array.isArray(object.basePoints)) {
-      if ('isClosed' in object) {
+    // see create for the required attributes and types
+    if ('basePoints' in object && 'transforms' in object && 'isCanonicalized' in object && 'isClosed' in object) {
+      // NOTE: transforms should be a TypedArray, which has a read-only length
+      if (Array.isArray(object.basePoints) && 'length' in object.transforms) {
         return true
       }
     }
