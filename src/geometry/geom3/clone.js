7802d56bb0d68815802d5f74c571349cf46f1c1c
Z3 Development <z3-dev@gfnews.net>
2019-04-18 16:48:49 +0900

    fix(geom3) changed clone() to perform DEEP clone of contents

--- src/geometry/geom3/clone.js
+++ src/geometry/geom3/clone.js
@@ -1,8 +1,21 @@
+const mat4 = require('../../math/mat4')
+
+const poly3 = require('../poly3')
+
+const create = require('./create')
+
 /**
- * Performs a shallow copy of the given geometry.
- * @params {geom3} geometry - the geometry to make a shallow copy of.
- * @returns {geom3} the copied geometry
+ * Performs a deep clone of the given geometry.
+ * @params {geom3} geometry - the geometry to clone
+ * @returns {geom3} a new geometry
  */
-const clone = (geometry) => Object.assign({}, geometry)
+const clone = (geometry) => {
+  let out = create()
+  out.polygons = geometry.polygons.map((polygon) => poly3.clone(polygon))
+  out.isCanonicalized = geometry.isCanonicalized
+  out.isRetesselated = geometry.isRetesselated
+  out.transforms = mat4.clone(geometry.transforms)
+  return out
+}
 
 module.exports = clone
