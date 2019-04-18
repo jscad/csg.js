e8f89d1fbb68d317fbe27ecd35129d20bd882e72
Z3 Development <z3-dev@gfnews.net>
2019-04-18 16:53:19 +0900

    tests(path2) corrected title of test

--- src/geometry/path2/transform.test.js
+++ src/geometry/path2/transform.test.js
@@ -8,7 +8,7 @@ const {transform, fromPoints, toPoints} = require('./index')
 
 const line = fromPoints({}, [[0, 0], [1, 0]])
 
-test('transform: An empty path produces an empty point array', t => {
+test('transform: An populated path produces an populated point array', t => {
   t.deepEqual(toPoints(transform(fromZRotation(degToRad(90)), line)),
               [vec2.fromValues(0, 0), vec2.fromValues(0, 1)])
 })
