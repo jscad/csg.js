15b39f2bc8da4ef1122a50c43fc91518ce80b24a
Z3 Development <z3-dev@gfnews.net>
2019-04-18 16:50:40 +0900

    fix(path2) corrected transform, set isCanonicalized to false

--- src/geometry/path2/transform.js
+++ src/geometry/path2/transform.js
@@ -1,20 +1,22 @@
 const mat4 = require('../../math/mat4')
 
-const clone = require('./clone')
+const create = require('./create')
 
 /**
  * A lazy transform of all of the points in the path.
- * @param {mat4} matrix - the matrix to transform with.
- * @param {path2} path - the path to transform.
- * @returns {path2} - the transformed path.
+ * @param {mat4} matrix - the matrix to transform with
+ * @param {path2} geometry - the path to transform
+ * @returns {path2} - the transformed path
  * @example
  * transform(fromZRotation(degToRad(90)), path)
  */
-const transform = (matrix, path) => {
-  let cloned = clone(path)
-  cloned.transforms = mat4.multiply(cloned.transforms, matrix)
-  cloned.isCanonicalized = false
-  return cloned
+const transform = (matrix, geometry) => {
+  let newgeometry = create(geometry.points) // reuse the points
+  newgeometry.isClosed = geometry.isClosed
+
+  newgeometry.transforms = mat4.multiply(geometry.transforms, matrix)
+  newgeometry.isCanonicalized = false
+  return newgeometry
 }
 
 module.exports = transform
