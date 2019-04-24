88de71282103730dcd24f8e6b606e35fb04bda44
Z3 Development <z3-dev@gfnews.net>
2019-04-24 09:23:10 +0900

    reorg(text) removed vector_char and vector_text, adjusted test suites

--- src/text/index.js
+++ src/text/index.js
@@ -1,6 +1,4 @@
 module.exports = {
-  vector_char: require('./vector_char'),
-  vector_text: require('./vector_text'),
   vectorChar: require('./vectorChar'),
   vectorText: require('./vectorText')
 }
