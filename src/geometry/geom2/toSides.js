59af709a67ff4d312a48d8b1b004ee708bed6a61
Z3 Development <z3-dev@gfnews.net>
2019-04-25 09:10:19 +0900

    fix(geom2) enhanced documentation, corrected toString to use toSides

--- src/geometry/geom2/toSides.js
+++ src/geometry/geom2/toSides.js
@@ -2,7 +2,7 @@ const applyTransforms = require('./applyTransforms')
 
 /**
  * Produces an array of sides from the given geometry.
- * The returned array should not be modified as the sides are shared with the geometry.
+ * The returned array should not be modified as the data is shared with the geometry.
  * @param {geom2} geometry - the geometry
  * @returns {Array} an array of sides, each side contains an array of two points
  * @example
--- src/geometry/geom2/toString.js
+++ src/geometry/geom2/toString.js
@@ -1,5 +1,7 @@
 const vec2 = require('../../math/vec2')
 
+const toSides = require('./toSides')
+
 /**
  * Create a string representing the contents of the given geometry.
  * @returns {String} a representive string
@@ -7,8 +9,9 @@ const vec2 = require('../../math/vec2')
  * console.out(toString(geometry))
  */
 const toString = function (geometry) {
-  let result = 'geom2 (' + geometry.sides.length + ' sides):\n[\n'
-  geometry.sides.forEach((side) => {
+  let sides = toSides(geometry)
+  let result = 'geom2 (' + sides.length + ' sides):\n[\n'
+  sides.forEach((side) => {
     result += '  [' + vec2.toString(side[0]) + ', ' + vec2.toString(side[1]) + ']\n'
   })
   result += ']\n'
