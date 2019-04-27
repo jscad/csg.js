04b23e5c630e1365fcb0a56499ccaa29497d2113
Z3 Development <z3-dev@gfnews.net>
2019-04-27 13:26:02 +0900

    tests(rectangle) completed tests for options

--- src/primitives/rectangle.test.js
+++ src/primitives/rectangle.test.js
@@ -7,14 +7,42 @@ const geom2 = require('../geometry/geom2')
 const comparePoints = require('../../test/helpers/comparePoints')
 
 test('rectangle (defaults)', t => {
+  const geometry = rectangle()
+  const obs = geom2.toPoints(geometry)
   const exp = [
     [-1, -1],
     [1, -1],
     [1, 1],
     [-1, 1]
   ]
-  const geometry = rectangle()
-  const obs = geom2.toPoints(geometry)
+
+  t.deepEqual(obs.length, 4)
+  t.true(comparePoints(obs, exp))
+})
+
+test('rectangle (options)', t => {
+  // test center
+  let geometry = rectangle({center: [-4, -4]})
+  let obs = geom2.toPoints(geometry)
+  let exp = [
+    [ -5, -5 ],
+    [ -3, -5 ],
+    [ -3, -3 ],
+    [ -5, -3 ]
+  ]
+
+  t.deepEqual(obs.length, 4)
+  t.true(comparePoints(obs, exp))
+
+  // test radius
+  geometry = rectangle({radius: [3, 5]})
+  obs = geom2.toPoints(geometry)
+  exp = [
+    [ -3, -5 ],
+    [ 3, -5 ],
+    [ 3, 5 ],
+    [ -3, 5 ]
+  ]
 
   t.deepEqual(obs.length, 4)
   t.true(comparePoints(obs, exp))
