d0558d11d86d5cbff86f2290fa31d865c975fb9d
Z3 Development <z3-dev@gfnews.net>
2019-05-11 17:32:58 +0900

    fix(rotate) changed to use mat4.fromTaitBryanRotation for matrix calculation

--- src/operations/transforms/rotate.js
+++ src/operations/transforms/rotate.js
@@ -4,46 +4,8 @@ const mat4 = require('../../math/mat4')
 
 const {geom2, geom3, path2} = require('../../geometry')
 
-// Tait-Bryan Euler angle convention using active, intrinsic rotations around the axes in the order z-y-x
-// angle of rotations
-//   yaw/Z rotation
-//   pitch/Y rotation
-//   roll/X rotation
-const rotation = (yaw, pitch, roll) => {
-  // convert to radians
-  yaw = yaw * Math.PI * (1.0 / 180.0)
-  pitch = pitch * Math.PI * (1.0 / 180.0)
-  roll = roll * Math.PI * (1.0 / 180.0)
-
-  // precompute sines and cosines of Euler angles
-  const sy = Math.sin(yaw);
-  const cy = Math.cos(yaw);
-  const sp = Math.sin(pitch);
-  const cp = Math.cos(pitch);
-  const sr = Math.sin(roll);
-  const cr = Math.cos(roll);
-
-  // create and populate rotation matrix
-  // left-hand-rule rotation
-  //const els = [
-  //  cp*cy, sr*sp*cy - cr*sy, sr*sy + cr*sp*cy, 0,
-  //  cp*sy, cr*cy + sr*sp*sy, cr*sp*sy - sr*cy, 0,
-  //  -sp, sr*cp, cr*cp, 0,
-  //  0, 0, 0, 1
-  //]
-  // right-hand-rule rotation
-  const els = [
-    cp*cy, cp*sy, -sp, 0,
-    sr*sp*cy - cr*sy, cr*cy + sr*sp*sy, sr*cp, 0,
-    sr*sy + cr*sp*cy, cr*sp*sy - sr*cy, cr*cp, 0,
-    0, 0, 0, 1
-  ]
-  return mat4.clone(els)
-}
-
 /**
  * Rotate the given object(s) using the given options (if any)
- * @see The paper by D. Rose, Rotations in Three-Dimensions (Tait-Bryan euler angles)
  * @param {Array} angles - angle of rotations about X, Y, and X axis
  * @param {Object|Array} objects - the objects(s) to rotate
  * @return {Object|Array} the rotated object(s)
@@ -53,12 +15,17 @@ const rotation = (yaw, pitch, roll) => {
  */
 const rotate = (angles, ...objects) => {
   if (!Array.isArray(angles)) throw new Error('angles must be an array')
-  if (angles.length != 3) throw new Error('angles must contain X, Y and Y values')
+  if (angles.length != 3) throw new Error('angles must contain X, Y and Z values')
 
   objects = flatten(objects)
   if (objects.length === 0) throw new Error('wrong number of arguments')
 
-  const matrix = rotation(angles[2], angles[1], angles[0])
+  // convert to radians
+  let yaw = angles[2] * Math.PI * (1.0 / 180.0)
+  let pitch = angles[1] * Math.PI * (1.0 / 180.0)
+  let roll = angles[0] * Math.PI * (1.0 / 180.0)
+
+  const matrix = mat4.fromTaitBryanRotation(yaw, pitch, roll)
 
   const results = objects.map((object) => {
     if (path2.isA(object)) return path2.transform(matrix, object)
