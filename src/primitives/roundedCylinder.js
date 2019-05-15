1d397fe5495788b291c4b487ed5853659cf59f39
Z3 Development <z3-dev@gfnews.net>
2019-05-15 13:23:01 +0900

    fix(roundedCylinder) enhanced to support roundedRadius, closing issue #21 corrected height of cylinder to be the height requested added tests for all options

--- src/primitives/roundedCylinder.js
+++ src/primitives/roundedCylinder.js
@@ -10,6 +10,7 @@ const poly3 = require('../geometry/poly3')
  * @param {Array} [options.start=[0,-1,0]] - start point of cylinder
  * @param {Array} [options.end=[0,1,0]] - end point of cylinder
  * @param {Number} [options.radius=1] - radius of rounded ends, must be scalar
+ * @param {Number} [options.roundradius=0.2] - radius of rounded edges
  * @param {Number} [options.resolution=defaultResolution3D] - number of polygons per 360 degree revolution
  * @returns {geom3} new 3D geometry
  *
@@ -18,6 +19,7 @@ const poly3 = require('../geometry/poly3')
  *   start: [0, -10, 0],
  *   end: [0, 10, 0],
  *   radius: 2,
+ *   roundRadius: 0.5
  * })
  */
 const roundedCylinder = function (options) {
@@ -25,19 +27,20 @@ const roundedCylinder = function (options) {
     start: [0, -1, 0],
     end: [0, 1, 0],
     radius: 1,
+    roundRadius: 0.2,
     resolution: defaultResolution3D
   }
-  let {start, end, radius, resolution} = Object.assign({}, defaults, options)
+  let {start, end, radius, roundRadius, resolution} = Object.assign({}, defaults, options)
 
-  if (resolution < 4) throw new Error('resolution should be greater then 4')
+  if (roundRadius > (radius - EPS)) throw new Error('roundRadius must be smaller then the radius')
 
-  // FIXME start and end should be the ends of the cylinder, but the not because the rounded corners are ADDITIONS
-  // FIXME basically, the start and end should be adjusted for the rounded corners
+  if (resolution < 4) throw new Error('resolution must be four or more')
 
-  let p1 = start
-  let p2 = end
+  let direction = vec3.subtract(end, start)
+  let length = vec3.length(direction)
 
-  let direction = vec3.subtract(p2, p1)
+  if ((2*roundRadius) > (length - EPS)) throw new Error('the cylinder height must be larger than twice roundRadius')
+
   let defaultnormal
   if (Math.abs(direction[0]) > Math.abs(direction[1])) {
     defaultnormal = vec3.fromValues(0, 1, 0)
@@ -45,12 +48,12 @@ const roundedCylinder = function (options) {
     defaultnormal = vec3.fromValues(1, 0, 0)
   }
 
-  let length = vec3.length(direction)
-  if (length < EPS) throw new Error('start and end do not define a significant distance')
-
-  let zvector = vec3.scale(radius, vec3.unit(direction))
+  let zvector = vec3.scale(roundRadius, vec3.unit(direction))
   let xvector = vec3.scale(radius, vec3.unit(vec3.cross(zvector, defaultnormal)))
   let yvector = vec3.scale(radius, vec3.unit(vec3.cross(xvector, zvector)))
+
+  vec3.add(start, start, zvector)
+  vec3.subtract(end, end, zvector)
 
   let qresolution = Math.floor(0.25 * resolution)
 
@@ -60,44 +63,47 @@ const roundedCylinder = function (options) {
     let angle = Math.PI * 2.0 * slice1 / resolution
     let cylinderpoint = vec3.add(vec3.scale(Math.cos(angle), xvector), vec3.scale(Math.sin(angle), yvector))
     if (slice1 > 0) {
-      // cylinder points:
+      // cylinder wall
       let points = []
-      points.push(vec3.add(p1, cylinderpoint))
-      points.push(vec3.add(p1, prevcylinderpoint))
-      points.push(vec3.add(p2, prevcylinderpoint))
-      points.push(vec3.add(p2, cylinderpoint))
+      points.push(vec3.add(start, cylinderpoint))
+      points.push(vec3.add(start, prevcylinderpoint))
+      points.push(vec3.add(end, prevcylinderpoint))
+      points.push(vec3.add(end, cylinderpoint))
       polygons.push(poly3.fromPoints(points))
+
       let prevcospitch, prevsinpitch
       for (let slice2 = 0; slice2 <= qresolution; slice2++) {
         let pitch = 0.5 * Math.PI * slice2 / qresolution
         let cospitch = Math.cos(pitch)
         let sinpitch = Math.sin(pitch)
         if (slice2 > 0) {
+          // cylinder rounding, start
           points = []
           let point
-          point = vec3.add(p1, vec3.subtract(vec3.scale(prevcospitch, prevcylinderpoint), vec3.scale(prevsinpitch, zvector)))
+          point = vec3.add(start, vec3.subtract(vec3.scale(prevcospitch, prevcylinderpoint), vec3.scale(prevsinpitch, zvector)))
           points.push(point)
-          point = vec3.add(p1, vec3.subtract(vec3.scale(prevcospitch, cylinderpoint), vec3.scale(prevsinpitch, zvector)))
+          point = vec3.add(start, vec3.subtract(vec3.scale(prevcospitch, cylinderpoint), vec3.scale(prevsinpitch, zvector)))
           points.push(point)
           if (slice2 < qresolution) {
-            point = vec3.add(p1, vec3.subtract(vec3.scale(cospitch, cylinderpoint), vec3.scale(sinpitch, zvector)))
+            point = vec3.add(start, vec3.subtract(vec3.scale(cospitch, cylinderpoint), vec3.scale(sinpitch, zvector)))
             points.push(point)
           }
-          point = vec3.add(p1, vec3.subtract(vec3.scale(cospitch, prevcylinderpoint), vec3.scale(sinpitch, zvector)))
+          point = vec3.add(start, vec3.subtract(vec3.scale(cospitch, prevcylinderpoint), vec3.scale(sinpitch, zvector)))
           points.push(point)
 
           polygons.push(poly3.fromPoints(points))
 
+          // cylinder rounding, end
           points = []
-          point = vec3.add(p2, vec3.add(vec3.scale(prevcospitch, prevcylinderpoint), vec3.scale(prevsinpitch, zvector)))
+          point = vec3.add(end, vec3.add(vec3.scale(prevcospitch, prevcylinderpoint), vec3.scale(prevsinpitch, zvector)))
           points.push(point)
-          point = vec3.add(p2, vec3.add(vec3.scale(prevcospitch, cylinderpoint), vec3.scale(prevsinpitch, zvector)))
+          point = vec3.add(end, vec3.add(vec3.scale(prevcospitch, cylinderpoint), vec3.scale(prevsinpitch, zvector)))
           points.push(point)
           if (slice2 < qresolution) {
-            point = vec3.add(p2, vec3.add(vec3.scale(cospitch, cylinderpoint), vec3.scale(sinpitch, zvector)))
+            point = vec3.add(end, vec3.add(vec3.scale(cospitch, cylinderpoint), vec3.scale(sinpitch, zvector)))
             points.push(point)
           }
-          point = vec3.add(p2, vec3.add(vec3.scale(cospitch, prevcylinderpoint), vec3.scale(sinpitch, zvector)))
+          point = vec3.add(end, vec3.add(vec3.scale(cospitch, prevcylinderpoint), vec3.scale(sinpitch, zvector)))
           points.push(point)
           points.reverse()
 
