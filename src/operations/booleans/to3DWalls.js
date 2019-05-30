b384adb5e8710e9dfcbc56e36854384408ad9ce4
Z3 Development <z3-dev@gfnews.net>
2019-05-30 10:59:07 +0900

    fix(booleans) small name change in to3DWalls

--- src/operations/booleans/to3DWalls.js
+++ src/operations/booleans/to3DWalls.js
@@ -2,7 +2,7 @@ const {vec3} = require('../../math')
 
 const {geom2, geom3, poly3} = require('../../geometry')
 
-const toPolygon3D = (z0, z1, side) => {
+const to3DWall = (z0, z1, side) => {
   const points = [
     vec3.fromVec2(side[0], z0),
     vec3.fromVec2(side[1], z0),
@@ -15,7 +15,7 @@ const toPolygon3D = (z0, z1, side) => {
 const to3DWalls = (options, geometry) => {
   let sides = geom2.toSides(geometry)
 
-  let polygons = sides.map((side) => toPolygon3D(options.z0, options.z1, side))
+  let polygons = sides.map((side) => to3DWall(options.z0, options.z1, side))
 
   let result = geom3.create(polygons)
   return result
