c877d82ea988706a90650426c10850d387df73a3
Z3 Development <z3-dev@gfnews.net>
2019-05-30 11:41:00 +0900

    fix(torus) renamed 'roti' option to 'innerRotation'

--- src/primitives/torus.js
+++ src/primitives/torus.js
@@ -1,6 +1,6 @@
-const {extrudeRotate} = require('../operations')
+const {extrudeRotate, rotateZ} = require('../operations')
 
-const circle = require('./circle')
+const {circle} = require('./ellipse')
 
 /** Construct a torus by revolving a small circle (inner) about the circumference of a large (outer) circle.
  * @param {Object} [options] - options for construction
@@ -8,7 +8,7 @@ const circle = require('./circle')
  * @param {Float} [options.outerRadius=4] - radius of large (outer) circle
  * @param {Integer} [options.innerSegments=16] - number of segments to create per 360 rotation
  * @param {Integer} [options.outerSegments=12] - number of segments to create per 360 rotation
- * @param {Integer} [options.roti=0] - rotation angle of base circle
+ * @param {Integer} [options.innerRotation=0] - rotation of small (inner) circle in radians
  * @returns {geom3} new 3D geometry
  *
  * @example
@@ -22,15 +22,15 @@ const torus = (options) => {
     innerSegments: 16,
     outerRadius: 4,
     outerSegments: 12,
-    roti: 0
+    innerRotation: 0
   }
-  let {innerRadius, innerSegments, outerRadius, outerSegments, roti} = Object.assign({}, defaults, options)
+  let {innerRadius, innerSegments, outerRadius, outerSegments, innerRotation} = Object.assign({}, defaults, options)
 
   if (innerRadius >= outerRadius) throw new Error('inner circle is two large to rotate about the outer circle')
 
   let innerCircle = circle({radius: innerRadius, center: [outerRadius, 0], segments: innerSegments})
 
-  //if (roti) innerCircle = innerCircle.transform(Matrix4x4.rotationZ(roti))
+  if (innerRotation !== 0) innerCircle = rotateZ(innerRotation, innerCircle)
 
   options = {
     startAngle: 0,
