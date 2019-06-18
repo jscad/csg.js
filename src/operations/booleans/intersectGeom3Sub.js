3b3c194a55d4177be0610f7137773c8ca439052d
Z3 Development <z3-dev@gfnews.net>
2019-06-18 13:50:26 +0900

    enhance(intersectGeom3Sub subtractGeom3Sub) check mayOverlap, and return a faster result

--- src/operations/booleans/intersectGeom3Sub.js
+++ src/operations/booleans/intersectGeom3Sub.js
@@ -1,15 +1,20 @@
 const {geom3} = require('../../geometry')
 
+const mayOverlap = require('./mayOverlap')
 const {Tree} = require('./trees')
 
 /**
- * Return a new 3D geometry representing space in both the first geometry and
- * in the second geometry. None of the given geometries are modified.
+ * Return a new 3D geometry representing the space in both the first geometry and
+ * the second geometry. None of the given geometries are modified.
  * @param {geom3} geometry1 - a geometry
  * @param {geom3} geometry2 - a geometry
  * @returns {geom3} new 3D geometry
  */
-const intersectSub = (geometry1, geometry2) => {
+const intersectGeom3Sub = (geometry1, geometry2) => {
+  if (!mayOverlap(geometry1, geometry2)) {
+    return geom3.create() // empty geometry
+  }
+
   let a = new Tree(geom3.toPolygons(geometry1))
   let b = new Tree(geom3.toPolygons(geometry2))
 
@@ -25,4 +30,4 @@ const intersectSub = (geometry1, geometry2) => {
   return geom3.create(newpolygons)
 }
 
-module.exports = intersectSub
+module.exports = intersectGeom3Sub
--- src/operations/booleans/subtractGeom3Sub.js
+++ src/operations/booleans/subtractGeom3Sub.js
@@ -1,16 +1,20 @@
 const {geom3} = require('../../geometry')
 
+const mayOverlap = require('./mayOverlap')
 const {Tree} = require('./trees')
 
 /**
- * Return a new 3D geometry representing space in the first geometry but not
- * in the second geometry.
- * None of the given geometries are modified.
+ * Return a new 3D geometry representing the space in the first geometry but not
+ * in the second geometry. None of the given geometries are modified.
  * @param {geom3} geometry1 - a geometry
  * @param {geom3} geometry2 - a geometry
  * @returns {geom3} new 3D geometry
  */
-const subtractSub = (geometry1, geometry2) => {
+const subtractGeom3Sub = (geometry1, geometry2) => {
+  if (!mayOverlap(geometry1, geometry2)) {
+    return geom3.clone(geometry1)
+  }
+
   let a = new Tree(geom3.toPolygons(geometry1))
   let b = new Tree(geom3.toPolygons(geometry2))
 
@@ -24,4 +28,4 @@ const subtractSub = (geometry1, geometry2) => {
   return geom3.create(newpolygons)
 }
 
-module.exports = subtractSub
+module.exports = subtractGeom3Sub
