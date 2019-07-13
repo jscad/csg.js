0eb26b8f71d5a503973805573d514ba89acb28e0
Z3 Development <z3-dev@gfnews.net>
2019-07-14 08:18:55 +0900

    fix(hullChain) removed options parameter (closed) as this was confusing

--- src/operations/hulls/hullChain.js
+++ src/operations/hulls/hullChain.js
@@ -4,23 +4,18 @@ const union = require('../booleans/union')
 
 const hull = require('./hull')
 
-/** Create a chain of hulled geometries from the given gemetries.
+/**
+ * Create a chain of hulled geometries from the given gemetries.
  * Essentially hull A+B, B+C, C+D, etc., then union the results.
- * Originally "Whosa whatsis" suggested "Chain Hull"
  * @param {...geometries} geometries - list of geometries from which to create hulls
  * @returns {geometry} new geometry
  *
  * @example:
  * let newshape = hullChain(rectangle({center: [-5,-5]}), circle({center: [0,0]}), rectangle({center: [5,5]}))
  */
-const hullChain = (options, ...geometries) => {
-  const defaults = { closed: false }
-  let { closed } = Object.assign({}, defaults, options)
-
+const hullChain = (...geometries) => {
   geometries = flatten(geometries)
   if (geometries.length < 2) throw new Error('wrong number of arguments')
-
-  if (closed === true && geometries[0] !== geometries[geometries.length - 1]) geometries.push(geometries[0])
 
   let hulls = []
   for (let i = 1; i < geometries.length; i++) {
--- src/operations/hulls/hullChain.test.js
+++ src/operations/hulls/hullChain.test.js
@@ -9,13 +9,13 @@ test('hullChain (two)', t => {
   let geometry2 = geom2.fromPoints([[-6, -6], [-9, -6], [-9, -9], [-6, -9]])
 
   // same
-  let obs = hullChain({}, geometry1, geometry1)
+  let obs = hullChain(geometry1, geometry1)
   let pts = geom2.toPoints(obs)
 
   t.is(pts.length, 4)
 
   // different
-  obs = hullChain({}, geometry1, geometry2)
+  obs = hullChain(geometry1, geometry2)
   pts = geom2.toPoints(obs)
 
   t.is(pts.length, 6)
@@ -26,14 +26,14 @@ test('hullChain (three)', t => {
   let geometry2 = geom2.fromPoints([[-6, -6], [-9, -6], [-9, -9], [-6, -9]])
   let geometry3 = geom2.fromPoints([[-6, 6], [-3, 6], [-3, 9], [-6, 9]])
 
-  // closed = false
-  let obs = hullChain({ closed: false }, geometry1, geometry2, geometry3)
+  // open
+  let obs = hullChain(geometry1, geometry2, geometry3)
   let pts = geom2.toPoints(obs)
 
   t.is(pts.length, 10)
 
-  // closed = true
-  obs = hullChain({ closed: true }, geometry1, geometry2, geometry3)
+  // closed
+  obs = hullChain(geometry1, geometry2, geometry3, geometry1)
   pts = geom2.toPoints(obs)
 
   t.is(pts.length, 10)
