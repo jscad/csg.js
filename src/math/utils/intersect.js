64a9dc00e8332e75f46c233c0709b30aabcf8a24
Z3 Development <z3-dev@gfnews.net>
2019-07-14 11:00:47 +0900

    fix(math) corrected utils/intersect to return undefined instead of null

--- src/math/utils/intersect.js
+++ src/math/utils/intersect.js
@@ -1,18 +1,18 @@
 // Calculate the intersect point of the two line segments (p1-p2 and p3-p4), end points included.
 // @see http://paulbourke.net/geometry/pointlineplane/
-// Note: If the line segments do NOT intersect then null is returned
+// Note: If the line segments do NOT intersect then undefined is returned
 // Return the intersection point of two line segments
 const intersect = (p1, p2, p3, p4) => {
   // Check if none of the lines are of length 0
   if ((p1[0] === p2[0] && p1[1] === p2[1]) || (p3[0] === p4[0] && p3[1] === p4[1])) {
-    return null
+    return undefined
   }
 
   denominator = ((p4[1] - p3[1]) * (p2[0] - p1[0]) - (p4[0] - p3[0]) * (p2[1] - p1[1]))
 
   // Lines are parallel
   if (Math.abs(denominator) < Number.MIN_VALUE) {
-    return null
+    return undefined
   }
 
   let ua = ((p4[0] - p3[0]) * (p1[1] - p3[1]) - (p4[1] - p3[1]) * (p1[0] - p3[0])) / denominator
@@ -20,7 +20,7 @@ const intersect = (p1, p2, p3, p4) => {
 
   // is the intersection along the segments
   if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
-    return null
+    return undefined
   }
 
   // Return the x and y coordinates of the intersection
