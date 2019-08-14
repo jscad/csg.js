a530a4661df2b82232871bdfc3c705de4c99fb0b
Z3 Development <z3-dev@gfnews.net>
2019-08-14 12:57:20 +0900

    reorg(math) added intersect() of line segments to utils

--- src/math/utils/index.js
+++ src/math/utils/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   clamp: require('./clamp'),
   degToRad: require('./degToRad'),
+  intersect: require('./intersect'),
   radToDeg: require('./radToDeg'),
   solve2Linear: require('./solve2Linear')
 }
