1e5702ee72b7b7cdba23c05631b38e75e798d6f4
Z3 Development <z3-dev@gfnews.net>
2019-08-17 09:16:18 +0900

    docs(connectors) updated documentation for the example

--- src/connectors/create.js
+++ src/connectors/create.js
@@ -1,18 +1,15 @@
 const vec3 = require('../math/vec3')
 
 /**
- * FIXME remove this and add to user guide
- * For example a servo motor and a servo horn:
- * Both can have a Connector called 'shaft'
- * The horn can be moved and rotated such that the two connectors match
- * and the horn is attached to the servo motor at the proper position.
- * Connectors are children of the solid, transform wise (parent => child relationship)
- * so they get the same transformations applied as the solid
- */
-
-/**
  * Create a new connector.
  * A connector allows two objects to be connected at predefined positions.
+ *
+ * For example a servo motor and a servo horn can both have a connector called 'shaft'.
+ * The horn can be moved and rotated to any position, and then the servo horn
+ * is attached to the servo motor at the proper position, such that the two connectors match.
+ * Connectors are children of the solid, transform-wise, so transformations are applied
+ * to both solid and connector(s).  (parent => child relationship)
+ *
  * @property {vec3} point - the position of the connector (relative to its parent)
  * @property {vec3} axis - the direction (unit vector) of the connector
  * @property {vec3} normal - the direction (unit vector) perpendicular to axis, that defines the "12 o'clock" orientation of the connector
