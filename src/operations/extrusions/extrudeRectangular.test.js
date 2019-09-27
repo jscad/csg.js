568e58556cf0c935b7e75d1d11bd9b1c6a6ead57
Z3 Development <z3-dev@gfnews.net>
2019-09-27 10:49:14 +0900

    fix(extrusions) updated test suites due to changes in expansions, corners

--- src/operations/extrusions/extrudeRectangular.test.js
+++ src/operations/extrusions/extrudeRectangular.test.js
@@ -8,41 +8,41 @@ const { arc, rectangle } = require('../../primitives')
 
 const extrudeRectangular = require('./extrudeRectangular')
 
-test('extrudeRectangular (defaults)', t => {
+test('extrudeRectangular (defaults, edge)', t => {
   let geometry1 = arc({ radius: 5, endAngle: degToRad(90) })
   let geometry2 = rectangle({ size: [5, 5] })
 
   let obs = extrudeRectangular({ }, geometry1)
   let pts = geom3.toPoints(obs)
-  t.is(pts.length, 42)
-
-  obs = extrudeRectangular({ }, geometry2)
-  pts = geom3.toPoints(obs)
-  t.is(pts.length, 32)
-})
-
-test('extrudeRectangular (segments = 1)', t => {
-  let geometry1 = arc({ radius: 5, endAngle: degToRad(90) })
-  let geometry2 = rectangle({ size: [5, 5] })
-
-  let obs = extrudeRectangular({ segments: 1 }, geometry1)
-  let pts = geom3.toPoints(obs)
   t.is(pts.length, 50)
 
-  obs = extrudeRectangular({ segments: 1 }, geometry2)
+  obs = extrudeRectangular({ }, geometry2)
   pts = geom3.toPoints(obs)
   t.is(pts.length, 40)
 })
 
-test('extrudeRectangular (segments = 8)', t => {
+test('extrudeRectangular (chamfer)', t => {
   let geometry1 = arc({ radius: 5, endAngle: degToRad(90) })
   let geometry2 = rectangle({ size: [5, 5] })
 
-  let obs = extrudeRectangular({ segments: 8 }, geometry1)
+  let obs = extrudeRectangular({ corners: 'chamfer' }, geometry1)
+  let pts = geom3.toPoints(obs)
+  t.is(pts.length, 42)
+
+  obs = extrudeRectangular({ corners: 'chamfer' }, geometry2)
+  pts = geom3.toPoints(obs)
+  t.is(pts.length, 32)
+})
+
+test('extrudeRectangular (segments = 8, round)', t => {
+  let geometry1 = arc({ radius: 5, endAngle: degToRad(90) })
+  let geometry2 = rectangle({ size: [5, 5] })
+
+  let obs = extrudeRectangular({ segments: 8, corners: 'round' }, geometry1)
   let pts = geom3.toPoints(obs)
   t.is(pts.length, 56)
 
-  obs = extrudeRectangular({ segments: 8 }, geometry2)
+  obs = extrudeRectangular({ segments: 8, corners: 'round' }, geometry2)
   pts = geom3.toPoints(obs)
   t.is(pts.length, 40)
 })
@@ -59,7 +59,7 @@ test('extrudeRectangular (holes)', t => {
     [[-5.00000, -5.00000], [-5.00000, 5.00000]]
   ])
 
-  let obs = extrudeRectangular({ radius: 2, offset: [0, 0, 15], segments: 16 }, geometry2)
+  let obs = extrudeRectangular({ radius: 2, offset: [0, 0, 15], segments: 16, corners: 'round' }, geometry2)
   let pts = geom3.toPoints(obs)
-  t.is(pts.length, 112)
+  t.is(pts.length, 122)
 })
