6bb6cb3210ec5ee5e91257b06425d6cf82a92eff
Z3 Development <z3-dev@gfnews.net>
2019-10-07 11:06:33 +0900

    fix(extrusions) changed slice.toString to use ES6 templates

--- src/operations/extrusions/slice/toString.js
+++ src/operations/extrusions/slice/toString.js
@@ -1,12 +1,7 @@
 const vec3 = require('../../../math/vec3/')
 
-const toString = (slice) => {
-  let result = `[`
-  slice.edges.forEach((edge) => {
-    result += `[${vec3.toString(edge[0])}, ${vec3.toString(edge[1])}], `
-  })
-  result += ']'
-  return result
-}
+const edgesToString = (edges) => edges.reduce((result, edge) => result += `[${vec3.toString(edge[0])}, ${vec3.toString(edge[1])}], `, '')
+
+const toString = (slice) => `[${edgesToString(slice.edges)}]`
 
 module.exports = toString
